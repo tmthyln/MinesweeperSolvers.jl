1e3791f02398433b98d620db72522ee185e95b3c
Documenter.jl <documenter@juliadocs.github.io>
2020-12-19 00:39:11 +0000

    build based on 7cdec8e

---
+++ latest/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Minesweeper-Solvers","page":"Home","title":"Minesweeper Solvers","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"This package provides a bunch of different minesweeper games and primitives, as well as a couple solvers of varying effectiveness.","category":"page"}]
+}
